4c9edf352ec5be8d7f07738f18ea41de80a130d5
othmanez314 <118020621+othmanez314@users.noreply.github.com>
2024-10-01 17:13:52 -0400

    Add files via upload

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  trailingSlash: true, // This ensures all paths have a trailing slash
+  output: 'standalone', // (Optional) Makes the build standalone, helpful for deployment
+};
 
 export default nextConfig;
