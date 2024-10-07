898503bb74a6231fe0cb5638c4caf6945c6b361e
Othmane Zizi <othmane@uni.minerva.edu>
2024-10-07 19:04:08 -0400

    ok

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -4,9 +4,7 @@ import GoogleProvider from "next-auth/providers/google";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import prisma from "@/lib/prisma"; // Adjust the import path as necessary
 
-export const dynamic = 'force-dynamic';
-
-export const authOptions: NextAuthOptions = {
+const authOptions: NextAuthOptions = {
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_ID!,
@@ -15,11 +13,6 @@ export const authOptions: NextAuthOptions = {
   ],
   adapter: PrismaAdapter(prisma),
   secret: process.env.NEXTAUTH_SECRET!,
-  // Remove or set the session strategy to 'database' to store sessions in the DB
-  // The default strategy is 'database' when using an adapter
-  // session: {
-  //   strategy: 'database',
-  // },
   callbacks: {
     async session({ session, user }) {
       // Attach the user ID to the session object
@@ -34,4 +27,5 @@ export const authOptions: NextAuthOptions = {
 
 // Next.js 13 App Router-compatible route handler
 const handler = NextAuth(authOptions);
+
 export { handler as GET, handler as POST };
